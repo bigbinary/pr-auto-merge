cab60a9527c1e2a6d4544efc966b3811c096064c
unnitallman <unnitallman@users.noreply.github.com>
2021-10-31 16:47:34 +0530

    test

--- auto_merge_pr.rb
+++ auto_merge_pr.rb
@@ -10,7 +10,11 @@ client = Octokit::Client.new(access_token: ENV["INPUT_GITHUB_TOKEN"])
 owner,repo = ENV["GITHUB_REPOSITORY"].split("/")
 pr_number  = ENV["GITHUB_REF"].split("/")[2].to_i
 
+# if status checks are still pending
 pr_id = GithubGraphql::PullRequest.find(owner, repo, pr_number).id
 GithubGraphql::EnablePRAutoMerge.mark! pr_id
 
-
+# if status checks have already completed and is successs
+if client.status(ENV["GITHUB_REPOSITORY"], ENV["GITHUB_HEAD_REF"])[:state] == "success"
+  client.merge_pull_request(repo, pr_number)
+end
