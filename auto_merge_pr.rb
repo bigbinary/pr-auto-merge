9af75b23ab946e3f35b12e186670a324958c44c8
unnitallman <unnitallman@users.noreply.github.com>
2021-10-31 16:36:39 +0530

    test

--- auto_merge_pr.rb
+++ auto_merge_pr.rb
@@ -8,7 +8,7 @@ p ENV
 client = Octokit::Client.new(access_token: ENV["INPUT_GITHUB_TOKEN"])
 
 owner,repo = ENV["GITHUB_REPOSITORY"].split("/")
-pr_number  = ENV["GITHUB_REF"].split("/")[2]
+pr_number  = ENV["GITHUB_REF"].split("/")[2].to_i
 
 pr_id = GithubGraphql::PullRequest.find(owner, repo, pr_number).id
 GithubGraphql::EnablePRAutoMerge.mark! pr_id
