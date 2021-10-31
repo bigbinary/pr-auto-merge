4559bcdecfb56c4feb42d85a6fa1f4a05e97f504
unnitallman <unnitallman@users.noreply.github.com>
2021-10-31 16:19:42 +0530

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM rubylang/ruby:2.7.2-bionic
 
 RUN gem install octokit
+RUN gem install graphql-client
 
 WORKDIR /app
 COPY . .
--- auto_merge_pr.rb
+++ auto_merge_pr.rb
@@ -1,13 +1,16 @@
 require 'octokit'
+require './src/github_graphql'
+require './src/pull_request'
+require './src/enable_pr_auto_merge'
 
 p ENV
 
 client = Octokit::Client.new(access_token: ENV["INPUT_GITHUB_TOKEN"])
 
-repo      = ENV["GITHUB_REPOSITORY"]
-pr_number = ENV["GITHUB_REF"].split("/")[2]
+owner,repo = ENV["GITHUB_REPOSITORY"].split("/")
+pr_number  = ENV["GITHUB_REF"].split("/")[2]
 
-if client.status(ENV["GITHUB_REPOSITORY"], ENV["GITHUB_HEAD_REF"])[:state] == "success"
-  client.merge_pull_request(repo, pr_number)
-end
+pr_id = GithubGraphQl::PullRequest.find(owner, repo, pr_number).id
+GithubGraphql::EnablePRAutoMerge.mark! pr_id
+
 
