8d334404a22cd1067ef219e37f463139bb607b56
unnitallman <unnitallman@users.noreply.github.com>
2021-10-31 11:58:12 +0530

    test

--- auto_merge_pr.rb
+++ auto_merge_pr.rb
@@ -1,10 +1,13 @@
 require 'octokit'
 
 p ENV
+
 client = Octokit::Client.new(access_token: ENV["INPUT_GITHUB_TOKEN"])
 
 repo      = ENV["GITHUB_REPOSITORY"]
 pr_number = ENV["GITHUB_REF"].split("/")[2]
 
-client.merge_pull_request(repo, pr_number)
+if client.status(ENV["GITHUB_REPOSITORY"], ENV["GITHUB_HEAD_REF"])[:state] == "success"
+  client.merge_pull_request(repo, pr_number)
+end
 
